2e48530ed209a15071b04b86added3a58d7f314a
Charlie <murdaresambhaji4@gmail.com>
2023-09-25 17:07:40 +0530

    exercises on if statrment

---
+++ if_statement.py
@@ -0,0 +1,13 @@
+
+price = 10000                  # House parice
+buyer_credit = False            # buyer credit good or bad
+
+if buyer_credit == True:       # if good creadit
+    price = (price * 0.1)
+
+
+elif buyer_credit == False:    # if bad credit 
+    price = (price * 0.2)
+
+print(f"Down payment: {price}") # print downpayment
+
