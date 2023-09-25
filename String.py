bbfbba493eb8766f850181c732d20ab93829e7bb
Charlie <murdaresambhaji4@gmail.com>
2023-09-25 16:23:24 +0530

    operation on string

---
+++ String.py
@@ -0,0 +1,20 @@
+String = "sambhaji is here"
+
+'operation on string'
+
+print(len(String))
+
+print(String.upper())
+
+print(String.lower())
+
+# make first latter upper case
+print(String.title())
+
+print(String.find('is'))
+
+print(String.replace("sambhaji","prasad"))
+
+# find any word in string
+
+print("sambhaji" in String)
