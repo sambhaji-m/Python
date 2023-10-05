c62ef929743a71a5e93b18890ff84c73f23cbbc9
Charlie <murdaresambhaji4@gmail.com>
2023-10-05 19:18:56 +0530

    new program

--- for.py
+++ for.py
@@ -1,5 +1,11 @@
-list = [10,20,30,40,50]
+list = [10,20,30,40,50] #calculate total from list
 total = 0
 for i in list:
     total = i+total
 print(total)
+
+# print * according to list value
+
+number = [5,2,5,2,2]
+for i in number:
+    print(i*"*")
