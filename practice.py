68b6a713b3773aecb268146f5722311eb911c353
Charlie <murdaresambhaji4@gmail.com>
2023-10-05 15:24:39 +0530

    make practice on comparision operator

---
+++ practice.py
@@ -0,0 +1,7 @@
+name = "sambhajiknaangnaengnegnlnegknklgnaekgnklaengkengknaekgnkaengkjlnaekjgnkeabgkjeabgkjlbeakgbkebgklaebgkljebkljbgkjaebkjgbaekbgkljaebgkjaebkjbekgjkaegbkbeg"
+if len(name) < 3:
+    print ("name must be at least 3 char")
+elif len(name)>50:
+    print("name cam be maximum of 50 char")
+else:
+    print("name looks good! ")
