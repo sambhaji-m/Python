3c8ac1a36bb84cec8b53b50a82053970e5976982
Charlie <murdaresambhaji4@gmail.com>
2023-10-05 15:34:06 +0530

    make them advance

--- pound_to_kg_convert.py
+++ pound_to_kg_convert.py
@@ -1,3 +1,16 @@
 pound = int(input("enter your weight in pound: "))
 kg = pound * 0.45
 print(kg)
+
+# next program
+
+weight = int(input("enter your weight: "))
+print("(L)bs or (K)g: ")
+choice = input("")
+
+if choice.upper == "L":
+    convert = weight * 0.25
+    print(f"you are {convert} kg")
+else:
+    convert = weight / 0.45
+    print(f"you are {convert} pound")
