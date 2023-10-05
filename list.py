54a2e3d2537b5bd15f46b48dba1a4c9afe146c06
Charlie <murdaresambhaji4@gmail.com>
2023-10-05 19:29:50 +0530

    operation on list

---
+++ list.py
@@ -0,0 +1,14 @@
+# find largest no from list
+list = [5,3,7,9,10,3,2]
+largest_no = list[0] 
+for i in list:
+    if i > largest_no:
+        largest_no = i
+print(largest_no)
+
+
+# using max build in function
+
+print(max(list))
+
+    
