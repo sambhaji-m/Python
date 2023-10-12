71754e19e8197778ba777e870ca96ca387b0f612
Charlie <murdaresambhaji4@gmail.com>
2023-10-12 14:57:03 +0530

    practice on random moduls

--- random_module.py
+++ random_module.py
@@ -1,10 +1,10 @@
-import random
-class Dice:
-    def roll(self):
+import random                                #import module
+class Dice:                                  # dice class
+    def roll(self):                          # Roll function
         first = random.randint(1,6)
         second = random.randint(1,6)
         return first,second
 
 
-dice = Dice()
+dice = Dice()                                # create object "dice" for execut the
 print(dice.roll())
