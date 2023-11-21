5205d60d42766e9e8ad55d7767b0fd4739a7bf50
Charlie <murdaresambhaji4@gmail.com>
2023-11-21 11:43:17 +0530

    class and objects

--- oop.py
+++ oop.py
@@ -25,3 +25,23 @@ class School:
 
 school = School
 School()                                        # call class
+
+# inheretance in python
+
+class A:
+    def program1(self):
+        print("sambhaji is here")
+        a = 5
+        b = 6
+
+class B(A):
+    def program2(self):
+        print ("prasad is here")
+
+a1 = A()
+b1 = B()
+
+
+a1.program1()
+b1.program1()
+b1.program2()
